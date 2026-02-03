202d61638de89a0444900f487e9a2595ad77f417
khushitiwari9151 <khushikhushitiwari01@gmail.com>
2026-02-03 15:34:07 +0530

    second commit

--- Area.java
+++ Area.java
@@ -1,6 +1,6 @@
 public class Area {
     public static void main(String[] args) {
         int a=5;
-        System.out.println("Area of circle of radius "+a+" is:"+3.14*a*a);
+        System.out.println("Area of circle of radius "+a+" is:"+34*a*a);
     }
 }
